f1a664f7deac28bca5746fb13e714ac549570ef8
eLBati <lb@takobi.online>
2021-02-16 17:36:52 +0100

    ADD sale_timesheet_portal

---
+++ sale_timesheet_portal/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Lorenzo Battistini @ TAKOBI
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sales Timesheet Portal",
+    "summary": "Show timesheet lines in portal sale orders",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Sales/Sales",
+    "website": "https://github.com/OCA/timesheet",
+    "author": "TAKOBI, Odoo Community Association (OCA)",
+    "maintainers": ["eLBati"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_timesheet",
+    ],
+    "data": [
+    ],
+}
